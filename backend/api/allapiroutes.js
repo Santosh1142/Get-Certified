89270d8a15d0a48519bb8caacf974ca4e9c78dee
NIKHILMOHAN063 <nikhilmohan063@gmail.com>
2021-06-07 18:03:03 +0530

    init

---
+++ backend/api/allapiroutes.js
@@ -0,0 +1,7 @@
+var express = require('express');
+const app = express.Router();
+
+
+
+
+module.exports=app;
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var mongoose= require('mongoose');
+//const bodyParser = require("body-parser");
+var path = require('path');
+var apis= require('./backend/api/allapiroutes.js');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//DataBase Connection
+var Connection_String="mongodb+srv://nikhil_mohan:uDiD2RTJNQMh3ghL@cluster0.ooac4.mongodb.net/Courses?retryWrites=true&w=majority";
+var options={useUnifiedTopology: true, useNewUrlParser: true };
+mongoose.connect(Connection_String,options,function cb(){
+    console.log(Connection_String);
+});
+mongoose.connection.on('connected', function()
+{console.log("Database Connected");})
+
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname+ '/frontend/html/resume.html'); 
+})
+
+app.use(express.static(__dirname+'/frontend'));
+app.use('/api',apis);
+
+app.get('/:page', function(req, res){
+    var ext = path.extname(req.params.page);
+    // console.log(ext);
+    if(ext=="")
+    res.sendFile(__dirname+ '/frontend/html/'+ req.params.page+".html");
+    //else  res.sendFile(__dirname+ '/frontend/'+ req.params.page);
+})
+
+var port= process.env.PORT  || 3000;
+app.listen(port,function cb()
+{console.log("http://localhost:"+port)
+});
