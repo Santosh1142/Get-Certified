5052b791368d961f10164b7b4625fe311e0550d1
NIKHILMOHAN063 <54111117+NIKHILMOHAN063@users.noreply.github.com>
2021-06-17 23:21:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const config = require("./backend/config");
 sgMail.setApiKey(config.SendgridAPIKey);
 var path = require('path');
 var apis= require('./backend/api/allapiroutes.js');
+var uis= require('./backend/ui/alluiroutes.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,18 +22,18 @@ mongoose.connection.on('connected', function()
 { console.log("GetCertified DataBase Connected");})
 
 
-// app.get('/', function(req, res){
-//    res.sendFile(__dirname+ '/frontend/html/resume.html'); 
-// })
-
-app.use(express.static(__dirname+'/frontend'));
-app.use('/api',apis);
 
 // var userauth= require("./backend/api/userauth");
 // app.use("/api/auth",userauth);
 
+app.use(express.static(__dirname+'/frontend'));
+app.use('/api',apis);
+app.use('/ui',uis);
 
-app.get('/:page', function(req, res){
+app.get('/', function(req, res){
+    res.sendFile(__dirname+ '/frontend/html/index.html'); 
+ })
+app.get('/page', function(req, res){
     var ext = path.extname(req.params.page);
     if(ext=="")
     res.sendFile(__dirname+ '/frontend/html/'+ req.params.page+".html");
@@ -44,18 +45,11 @@ app.listen(port,function cb()
 {console.log("http://localhost:"+port)
 });
 
-
-
-// var sampleData = [
-//     { contestname : "CodeForces", userid : 1234,userName: "Nikhil" },
-//     { contestname : "CodeChef", userid : 1143, userName : "Pranay" },
-//     { contestname : "HackerRank", userid : 7675, userName : "Kaushik" }
-// ];
-
-// app.get('/api/sample',(req,res)=>{
-//     res.json(sampleData)
-// })
-// app.post('/api/sample/:u',(req,res)=>{
-//     // console.log(req.body.data);
-//     sampleData.push(req.body.data)
-// })
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
+  });
